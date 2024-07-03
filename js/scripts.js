d8ff4114e3e80c79f8fdccd03acd918f804070ea
ethan2106 <jimdonne1609@gmail.com>
2024-07-03 09:46:10 +0200

    ajout favicon

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
   console.log("Document ready");
- // Ajoute un défilement doux à tous les liens ancrés
+
+  // Ajoute un défilement doux à tous les liens ancrés
   $('a[href*="#"]').on('click', function(e) {
     e.preventDefault();
-    $('html, body').animate({
-      scrollTop: $($(this).attr('href')).offset().top
-    }, 600, 'linear'); // Durée et style d'animation
+
+    var target = $(this).attr('href');
+    
+    // Vérifie si href n'est pas vide
+    if (target && target !== '#') {
+      // Vérifie si l'élément cible existe
+      if ($(target).length) {
+        $('html, body').animate({
+          scrollTop: $(target).offset().top
+        }, 600, 'linear');
+      } else {
+        console.log("Element cible non trouvé : " + target);
+      }
+    }
   });
 });
