b5021b533df3f63d31b1499b8cd12190024779c4
ethan2106 <jimdonne1609@gmail.com>
2024-07-13 21:33:37 +0200

    .

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,21 @@ $(document).ready(function() {
     }
   });
 }); // Closing bracket added to fix '}' expected error
+$(document).ready(function() {
+  // Fonction existante pour le défilement doux
+  // ...
+
+  // Nouvelle fonction pour le bouton de retour en haut
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 300) {
+      $('#back-to-top').fadeIn();
+    } else {
+      $('#back-to-top').fadeOut();
+    }
+  });
+
+  $('#back-to-top').click(function() {
+    $('html, body').animate({scrollTop : 0}, 800);
+    return false;
+  });
+});
